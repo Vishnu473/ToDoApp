245a36c096236d784ccf1af7daa6593742d3368d
Vishnu Kishore <tarinivishnukishore123@gmail.com>
2024-01-31 18:18:49 +0530

    Added Android, iOS files

---
+++ ToDoApp.Android/Sqlite_Android.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using ToDoApp.Droid;
+using ToDoApp.Model;
+using Xamarin.Forms;
+
+[assembly:Dependency(typeof(Sqlite_Android))]
+namespace ToDoApp.Droid
+{
+	public class Sqlite_Android : ISqlLiteConnection
+	{
+        public SQLiteConnection GetConnection()
+        {
+            var sqlFileName = "MyDatabase.db3";
+            var FilePath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var dbPath = Path.Combine(FilePath, sqlFileName);
+            return new SQLiteConnection(dbPath);
+        }
+    }
+}
+
---
+++ ToDoApp.iOS/Sqlite_iOS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using ToDoApp.Model;
+using ToDoApp.iOS;
+
+[assembly:Dependency(typeof(Sqlite_iOS))]
+namespace ToDoApp.iOS
+{
+	public class Sqlite_iOS : ISqlLiteConnection
+	{
+        public SQLiteConnection GetConnection()
+        {
+            var sqlFileName = "MyDataBBase.db3";
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var liraryPath = Path.Combine(documentsPath, "..", "Library");
+            var dbPath = Path.Combine(liraryPath, sqlFileName);
+            return new SQLiteConnection(dbPath);
+        }
+    }
+}
+
